83b5458ecc28a460c24ab12a70224c2a0843129b
Shigasuresh@123 <shigasuresh546@gmail.com>
2019-12-02 21:37:19 +0530

    Add Siri suggestion using UserActivities

--- SiriSuggestion/AppDelegate.swift
+++ SiriSuggestion/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if userActivity?.activityType == "Shigas.SiriSuggestion.shownotifications" {
+            NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "ShowNotifications")))
+        }
+        
         return true
     }
 
--- SiriSuggestion/ViewController.swift
+++ SiriSuggestion/ViewController.swift
@@ -7,14 +7,56 @@
 //
 
 import UIKit
+import IntentsUI
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, INUIAddVoiceShortcutViewControllerDelegate {
+    func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+        
+    }
+    
+    func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+        
+    }
+    
+    
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.displayNotifications), name: NSNotification.Name(rawValue: "ShowNotifications"), object: nil)
+        
+        setupIntentsForSiri()
     }
-
-
+    
+    func setupIntentsForSiri()  {
+        
+        let actionIdentifier = "Shigas.SiriSuggestion.shownotifications"
+        let activity = NSUserActivity(activityType: actionIdentifier)
+        activity.title = "Show Notifictions"
+        activity.userInfo = ["speech" : "show notifications"]
+        activity.isEligibleForSearch = true
+        
+        if #available(iOS 12.0, *) {
+            activity.isEligibleForPrediction = true
+            activity.persistentIdentifier = NSUserActivityPersistentIdentifier(actionIdentifier)
+        }
+        
+        view.userActivity = activity
+        activity.becomeCurrent()
+    }
+    
+    @objc func displayNotifications() {
+        if #available(iOS 12.0, *) {
+            guard let userActivity = view.userActivity else { return }
+            let shortcut = INShortcut(userActivity: userActivity)
+            let vc = INUIAddVoiceShortcutViewController(shortcut: shortcut)
+            vc.delegate = self
+            present(vc, animated: true, completion: nil)
+            
+        }
+    }
 }
 
